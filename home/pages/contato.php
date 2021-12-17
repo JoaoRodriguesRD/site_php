17179d4b541a391aead0f9179d665af0b3503143
joaorodriguesrd <j_neto_rodrigues@hotmail.com>
2021-12-17 19:38:02 -0300

    criando contato

---
+++ home/pages/contato.php
@@ -0,0 +1,15 @@
+<section class="contato">
+        <div class="center">
+            <form >
+                <h2>
+                    Contate-me
+                </h2>
+                <input type="text" name="nome" required placeholder="seu nome" />
+                <input type="email" name="email" required placeholder="email" />
+                <input type="text" name="telefone" required placeholder="telefone" />
+                <textarea name="mensagem" placeholder="mensagem"></textarea>
+                <input type="submit" name="enviar" value="Enviar!" />
+            </form>
+        </div>
+</section>
+<div style="position: fixed; width: 100%; bottom: 0;">
