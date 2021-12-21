25524e9fcba61dad97ac1bf48def2bbdc8b71df3
joaorodriguesrd <j_neto_rodrigues@hotmail.com>
2021-12-21 17:10:40 -0300

    autoload external class

--- home/config.php
+++ home/config.php
@@ -1,3 +1,8 @@
 <?php
-    define('INCLUDE_PATH','http://localhost/site_php/home/')
+    $autoload = function($class){
+        include('external/'.$class.'.php');
+    };
+    spl_autoload_register($autoload);
+    define('INCLUDE_PATH','http://localhost/site_php/home/');
+
 ?>
