f23ca2b8022393e8d76442c52fe60d91f8dae263
joaorodriguesrd <j_neto_rodrigues@hotmail.com>
2021-12-21 18:28:44 -0300

    melhorando autoload de classes

--- home/config.php
+++ home/config.php
@@ -1,12 +1,6 @@
 <?php
     $autoload = function($class){
-        if($class == 'Email'){
-            echo 'confusing screamming';
-            include('Email'.'.php');
-        }
-        //include('vendor/phpmailer/phpmailer/src/'.$class.'.php');
-
-        
+        include($class.'.php');
     };
     spl_autoload_register($autoload);
     define('INCLUDE_PATH','http://localhost/site_php/home/');
